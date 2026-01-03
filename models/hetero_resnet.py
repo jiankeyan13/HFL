d834f9e5ed65beed8d6b50ad988e9cb1431cadf6
YanShisan <649471913@qq.com>
2026-01-03 10:39:23 +0800

    异构网络

--- models/hetero_resnet.py
+++ models/hetero_resnet.py
@@ -20,18 +20,14 @@ class BasicBlock(nn.Module):
         )
         self.scaler1 = Scaler(scale)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
-        )
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.scaler2 = Scaler(scale)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False
-                ),
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
                 Scaler(scale),
                 nn.BatchNorm2d(self.expansion * planes),
             )
@@ -56,9 +52,7 @@ class Bottleneck(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.scaler1 = Scaler(scale)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-        )
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.scaler2 = Scaler(scale)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
@@ -68,9 +62,7 @@ class Bottleneck(nn.Module):
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False
-                ),
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
                 Scaler(scale),
                 nn.BatchNorm2d(self.expansion * planes),
             )
